8562c71b6a7d3577ef1fd7cda87113713d0faed4
Neha Priyadarshini <npriyadarshini@twitter.com>
2022-04-25 16:56:17 -0700

    adding tag info

--- src/components/sitevisit.jsx
+++ src/components/sitevisit.jsx
@@ -1,4 +1,4 @@
-export default function Purchase() {
+export default function Sitevisit() {
     return (
       <main style={{ padding: '1rem 0' }}>
         <h2>Lets purchase this and all other stuff!!</h2>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import SiteVisit from "./components/sitevisit";
+import Home from "./components/home";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router basename="/perfadcoolbeans3">
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="sitevisit" element={<SiteVisit />} />
+        <Route path="home" element={<Home />} />
+      </Route>
+    </Routes>
+  </Router>,
+  //<App />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
